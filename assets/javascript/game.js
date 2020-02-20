7319c7b70c74c6670e2d1b6989d02508966eff6f
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-02-20 15:25:10 -0700

    fifth commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,12 @@
 
 // Create an array with my list of words
-var wordlist=["Pulp Fiction", "Big Lebowski", "Fight Club", "Pretty Woman","Titanic","The Matrix", "Forrest Gump", 
-            "Scream", "American Beauty", "Magnolia","True Lies","Fargo","The Sixth Sense","The Mask","Braveheart"];
+var wordlist=["PULP FICTION", "BIG LEBOWSKI", "FIGHT CLUB", "PRETTY WOMAN","TITANIC","THE MATRIX", "FORREST GUMP", 
+            "SCREAM", "AMERICAN BEAUTY", "MAGNOLIA","TRUE LIES","FARGO","THE SIXTH SENSE","THE MASK","KILL BILL"];
+var letters=["A", "B", "C", "D", "F", "G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];            
 
 //Create a variable that picks the words randomly
 
-var wordlist= wordlist[Math.floor(Math.random()* wordlist.length)];
+var wordplay= wordlist[Math.floor(Math.random()* wordlist.length)];
 
 // Create an array that lets the user know how many letters the word have and loop to go through the wordlist array
 var wordspaces=[];
