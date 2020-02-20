929bae48cd61b06430a4c7125ada6068bf5f8d94
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-02-20 10:13:10 -0700

    third commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,14 @@
-var words=["tired", "confused","stressed"];
- console.log (words.length);
+
+// Create an array with my list of words
+var wordlist=["Pulp Fiction", "Big Lebowski", "Fight Club", "Pretty Woman","Titanic","The Matrix", "Forrest Gump", 
+            "Scream", "American Beauty", "Magnolia","True Lies","Fargo","The Sixth Sense","The Mask","Braveheart"];
+
+//Create a variable that picks the words randomly
+
+var wordlist= wordlist[Math.floor(Math.random()* wordlist.length)];
+
+// Create an array that lets the user know how many letters the word have and loop to go through the wordlist array
+var wordspaces=[];
+for (var i=0; i<wordlist.length;i++){
+    wordspaces[i]="_";
+}
