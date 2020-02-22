d1cce05f15f4d372cd9ae65b2da729b765af4455
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-02-21 23:51:21 -0700

    main game first step up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,15 @@
 // Create an array with my list of words
 var wordlist=["PULP FICTION", "BIG LEBOWSKI", "FIGHT CLUB", "PRETTY WOMAN","TITANIC","THE MATRIX", "FORREST GUMP", 
             "SCREAM", "AMERICAN BEAUTY", "MAGNOLIA","TRUE LIES","FARGO","THE SIXTH SENSE","THE MASK","KILL BILL"];
+            
+            
+// Hints to be used in each guess attemp for the specific word. 
+
+var hints =[{
+
+}
+
+]
 
 //create an array with the alphabet letters and variables to generate buttons
 var letters=["A", "B", "C", "D", "F", "G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];            
@@ -31,10 +40,49 @@ var result;
 
 //******************************** THE MAIN HANGMAN GAME ****************************************
 
+//create a function that runs the game:
 
-document.getElementById("guessesleft")
+function gameStart() {
+    randomword= wordlist[Math.floor(Math.random()* wordlist.length)];
+    console.log (randomword);
+
+    randomgame=wordlist[randomword];
+    gameletters= randomgame.split(' ');
+    guessesleft=randomgame.length;
+    dashes= randomgame.length;
+
+    if(guessesleft>10){
+        guessesleft=9;
+    }
+    for(let i=0; i<dashes; i++){
+        if(gameletters[i]===" ")
+        underscore.push(letters);
+
+    }else{
+        underscore.push("_");
+    }
+}
+//console.log the info of the game:
+console.log(gameletters);
+console.log (underscore);
 
 
-//Create a variable that picks the words randomly
+// Now i connect all the variables to the HTML file
 
-var randomword= wordlist[Math.floor(Math.random()* wordlist.length)];
+//allows for spaces between the underscore
+document.getElementById("wordspaces").textContent=underscore.join(' ');
+
+//keep the number of guesses left
+document.getElementById("guessesleft").textContent = guessesleft;
+
+//the wins and losses
+
+document.getElementById("wins").textContent = wins;
+document.getElementById("losses").textContent =losses;
+
+//to put the pictures and the hints up:
+
+
+letterbuttons=true;
+
+
